3ee53a45c3e7ad6e16882faf7b348929969f10af
oreldm <orel@mip.me>
2020-07-01 01:59:31 -0400

    transmitting filebeat throw logstash instead of directly to elastic

--- dateappWithBeat/script.sh
+++ dateappWithBeat/script.sh
@@ -1,1 +1,1 @@
-service ssh start; service filebeat start; while true; do date >> /app/time.txt ; sleep 5; rm /app/time.txt ; done
+service ssh start; service filebeat start; while true; do echo "{\"theDate\": \"" `date`  "\"}"  >> /app/date.log ; sleep 5; rm /app/date.log ; done
